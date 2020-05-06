3c9a2a191fb7e4e2dbef8416fb3e13d311e51888
Theracon <54535899+Theracon@users.noreply.github.com>
2020-05-06 11:48:28 +0100

    revert back to 3ed4e38a245d9c16d2c1bcfcc085f24566acdda7

--- app.js
+++ app.js
@@ -1,2 +1,1 @@
 console.log('This is a git app');
-console.log('This is aanother console.log');
--- cats.js
+++ cats.js
@@ -1,3 +1,1 @@
 console.log('This is a git app');
-console.log('This is aanother console.log');
-varm fgkala = rewasasa;
